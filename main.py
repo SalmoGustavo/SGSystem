547cbd1cc9642171795714f06792def00fb44736
salmogustavo <salmogustavo.business@gmail.com>
2024-05-12 17:46:10 -0300

    Ok

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+class App:
+    def __init__(self):
+        self.running = True
+
+    def run(self):
+        while self.running:
+            pass
+
+        exit()
+
+app = App()
+
+if __name__ == '__main__':
+    app.run()
